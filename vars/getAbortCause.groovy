5d6cd756132db801f73c0b6c6ee79c9e00eb61b1
mbaitelman <mendy@baitelman.com>
2021-04-08 20:11:53 -0700

    Update getAbortCause.groovy

--- vars/getAbortCause.groovy
+++ vars/getAbortCause.groovy
@@ -1,3 +1,3 @@
 def call(){
-  return currentBuild.getRawBuild().getActions(jenkins.model.InterruptedBuildAction).getCauses().getDisplayName()
+  return currentBuild.getRawBuild().getActions(jenkins.model.InterruptedBuildAction).getCauses()[0].getDisplayName()
 }
